8252c3928ce187e1eb1b6c9b5da343528e08ff2f
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-06-16 17:43:10 +0200

    added test cases for set valid registration numbers, add valid registration numbers

---
+++ unit-testing/registration-numbers.test.js
@@ -0,0 +1,66 @@
+describe('The RegistrationApp factory function unit testing', () => {
+    let app;
+
+    beforeEach(() => {
+        app = RegistrationApp();
+    });
+
+    describe('set valid registration numbers', () => {
+
+        it('should be able to return a valid registration number', () => {
+
+            app.setRegistrationNumber('ca 534-454').validRegistrationNumber();
+            assert.equal('CA 534-454', app.getRegistrationNumber());
+        });
+
+        it('should be able to return another valid registration number', () => {
+
+            app.setRegistrationNumber('cl 7484').validRegistrationNumber();
+            assert.equal('CL 7484', app.getRegistrationNumber());
+        });
+
+        it('should be able to show an error message when the registration is not valid', () => {
+            app.setRegistrationNumber('ca 53').validRegistrationNumber();
+            assert.equal('Please enter a valid registration number. e.g CA 435-546, CL 657, CJ 75755', app.getMessage());
+        });
+    });
+
+    describe('add a valid registration number to towns object', () => {
+
+        it('should be able to add a registration number to a certain town array', () => {
+            app.setRegistrationNumber('cl 7464').validRegistrationNumber();
+            app.getRegistrationNumber();
+            app.addRegistrationTownsObject();
+    
+            assert.deepEqual({ CA: [], CL: [ 'CL 7464' ], CJ: [] }, app.getTownRegistration());
+        });
+
+        it('should be able to add two registration number to a certain town array', () => {
+            app.setRegistrationNumber('ca 763-664').validRegistrationNumber();
+            app.getRegistrationNumber();
+            app.addRegistrationTownsObject();
+
+            app.setRegistrationNumber('ca 554').validRegistrationNumber();
+            app.getRegistrationNumber()
+            app.addRegistrationTownsObject();
+    
+            assert.deepEqual({ CA: ['CA 763-664', 'CA 554'], CL: [], CJ: [] }, app.getTownRegistration());
+        });
+
+        it('should not be able to add a duplicate of a registration number', () => {
+            app.setRegistrationNumber('ca 534').validRegistrationNumber();
+            app.getRegistrationNumber()
+            app.addRegistrationTownsObject();
+
+            app.setRegistrationNumber('ca 534').validRegistrationNumber();
+            app.getRegistrationNumber()
+            app.addRegistrationTownsObject();
+
+            assert.equal('CA 534 registration number already entered.', app.getMessage());
+        });
+    });
+
+    describe('filtering testing', () => {
+        
+    });
+});
